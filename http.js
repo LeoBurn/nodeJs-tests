cb8e1e46e1c921d5a50099b3ee66cbf90a066662
LeandroViana <leandroviana1988@gmail.com>
2017-06-29 22:45:19 +0100

    First Custom Module

--- http.js
+++ http.js
@@ -1,7 +1,8 @@
 var http = require('http');
+var dateTimeModule = require('./custom-modules/dateTimeModule.js');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-    console.log("received Request from: "+req.connection.remoteAddress);
+    res.write("The date and time are currently: " + dateTimeModule.myDateTime());
+    res.end();
 }).listen(8080);
