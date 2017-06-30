0e976a9847b82f04fd2206286125befdc6c2785d
LeandroViana <leandroviana1988@gmail.com>
2017-06-30 18:39:23 +0100

    File Read with routes

--- http.js
+++ http.js
@@ -1,11 +1,20 @@
 var http = require('http');
 var dateTimeModule = require('./custom-modules/dateTimeModule.js');
 var fileSystemModule = require('fs');
+var urlModule = require('url');
 
 http.createServer(function (request, response) {
-  fileSystemModule.readFile('./views/index.html',function(error,data){
-      response.writeHead(200, {'Content-Type':'text/html'});
-      response.write(data);
-      response.end();
-  })
+    var query = urlModule.parse(request.url, true);
+    var fileName = "./views/" + query.path;
+    fileSystemModule.readFile(fileName, function (error, data) {
+        if (error) {
+            response.writeHead(404, { 'Content-Type': 'text/html' });
+            return response.end("404 Not Found");
+        }
+
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
+    })
+
 }).listen(8080);
